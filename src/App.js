4ede22ba036af7b6c292458f3db15ade2ed4bf0f
Rachel Isaac <rachelisaac995@gmail.com>
2022-09-13 00:09:47 +0400

    feat: created the JSX content for the form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+
 import MainComponent from "./Components/mainComponent";
 import FrontDebitCardComponent from "./Components/frontDebitCardComponent";
 import BackDebitCardComponent from "./Components/backDebitCardComponent";
--- src/Components/mainComponent.js
+++ src/Components/mainComponent.js
@@ -1,10 +1,43 @@
 import React from "react";
+import "./mainComponent.css";
 
 const MainComponent = () => {
   return (
-    <div>
-      <h1>hello there</h1>
-    </div>
+    <>
+      <div className="whole-container">
+        <div className="upper-section"></div>
+        <div className="lower-section">
+          <p className="card-desc-tags">CARDHOLDER NAME</p>
+          <input
+            type="text"
+            name="name"
+            id=""
+            placeholder="e.g. Jane Appleseed"
+          />
+        </div>
+        <div>
+          <p className="card-desc-tags">CARD NUMBER</p>
+          <input
+            type="number"
+            name="number"
+            id=""
+            placeholder="e.g. 1234 5678 9123 0000"
+          />
+        </div>
+        <div>
+          <p className="card-desc-tags">EXP. DATE (MM/YY)</p>
+          <div>
+            <input type="number" name="number" id="" placeholder="MM" />
+            <input type="number" name="number" id="" placeholder="YY" />
+          </div>
+          <div>
+            <p className="card-desc-tags">CVC</p>
+            <input type="number" name="number" id="" placeholder="e.g. 123" />
+          </div>
+        </div>
+        <button className="btn">Confirm</button>
+      </div>
+    </>
   );
 };
 
