bf7fd86f26e8ec583f4b82818502c636290ed9c4
Rachel Isaac <rachelisaac995@gmail.com>
2022-09-15 17:55:14 +0400

    feat: built the jsx for debit card and form

--- src/Components/frontDebitCardComponent.js
+++ src/Components/frontDebitCardComponent.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./frontDebitCardComponent.css";
 
 const FrontDebitCardComponent = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="mainCard">
+        <div className="circles">
+          <div className="bigCircle"></div>
+          <div className="smallCircle"></div>
+        </div>
+        <p className="card-number">0000 0000 0000 0000</p>
+        <div className="name-and-date">
+          <p className="name">JANE APPLESEED</p>
+          <p className="date">00/00</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FrontDebitCardComponent;
--- src/Components/mainComponent.js
+++ src/Components/mainComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./mainComponent.css";
+import "./MainComponent.css";
 
 const MainComponent = () => {
   return (
@@ -7,35 +7,52 @@ const MainComponent = () => {
       <div className="whole-container">
         <div className="upper-section"></div>
         <div className="lower-section">
-          <p className="card-desc-tags">CARDHOLDER NAME</p>
-          <input
-            type="text"
-            name="name"
-            id=""
-            placeholder="e.g. Jane Appleseed"
-          />
-        </div>
-        <div>
-          <p className="card-desc-tags">CARD NUMBER</p>
-          <input
-            type="number"
-            name="number"
-            id=""
-            placeholder="e.g. 1234 5678 9123 0000"
-          />
-        </div>
-        <div>
-          <p className="card-desc-tags">EXP. DATE (MM/YY)</p>
-          <div>
-            <input type="number" name="number" id="" placeholder="MM" />
-            <input type="number" name="number" id="" placeholder="YY" />
+          <div className="user-card-heading">
+            <p className="card-desc-tags">CARDHOLDER NAME</p>
+            <input
+              type="text"
+              name="name"
+              id=""
+              placeholder="e.g. Jane Appleseed"
+            />
           </div>
-          <div>
-            <p className="card-desc-tags">CVC</p>
-            <input type="number" name="number" id="" placeholder="e.g. 123" />
+
+          <div className="user-card-heading">
+            <p className="card-desc-tags">CARD NUMBER</p>
+            <input
+              type="number"
+              name="number"
+              id=""
+              placeholder="e.g. 1234 5678 9123 0000"
+            />
           </div>
+          <div className="user-card-heading user-card-date">
+            <div className="card-date-input">
+              <p className="card-desc-tags">EXP. DATE (MM/YY)</p>
+              <div className="card-input-month-year">
+                <input
+                  type="number"
+                  name="number"
+                  id=""
+                  placeholder="MM"
+                  className="date-input"
+                />
+                <input
+                  type="number"
+                  name="number"
+                  id=""
+                  placeholder="YY"
+                  className="date-input"
+                />
+              </div>
+            </div>
+            <div className="user-card-cvc">
+              <p className="card-desc-tags">CVC</p>
+              <input type="number" name="number" id="" placeholder="e.g. 123" />
+            </div>
+          </div>
+          <button className="btn">Confirm</button>
         </div>
-        <button className="btn">Confirm</button>
       </div>
     </>
   );
