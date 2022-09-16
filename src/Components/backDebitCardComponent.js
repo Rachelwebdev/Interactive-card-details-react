ffd2605095587ec37d5d65af2277340d8596c860
Rachel Isaac <rachelisaac995@gmail.com>
2022-09-17 02:03:03 +0400

    feat: built the jsx for the backDebitCard

--- src/Components/backDebitCardComponent.js
+++ src/Components/backDebitCardComponent.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./backDebitCardComponent.css";
 
 const BackDebitCardComponent = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="mainBackCard">
+        <div className="black-section"></div>
+        <div className="cvc-section">000</div>
+      </div>
+    </>
+  );
 };
 
 export default BackDebitCardComponent;
